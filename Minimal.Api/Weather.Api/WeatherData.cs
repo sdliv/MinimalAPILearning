e5f7bd4eba04b0ec1bfc31aecfb2dd52447417eb
Sean Livingston <sean.d.livingston@gmail.com>
2022-11-25 13:38:51 -0500

    Adding Untracked Files. Nothing changes from initial commit.

---
+++ Minimal.Api/Weather.Api/WeatherData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Weather.Api
+{
+	public class WeatherData
+	{
+		public static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Swelter"
+		};
+		public WeatherData()
+		{
+		}
+	}
+}
+
