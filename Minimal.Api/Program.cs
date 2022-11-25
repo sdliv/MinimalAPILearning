58e7b22a06cad6446184b87eb1c9882ad3e65ac3
Sean Livingston <sean.d.livingston@gmail.com>
2022-11-25 13:52:47 -0500

    Removes Weather.Api

--- Minimal.Api/Program.cs
+++ Minimal.Api/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 //Configure our service
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.Urls.Add("https://localhost:7777");
+app.Urls.Add("https://localhost:8888");
 app.Run();
